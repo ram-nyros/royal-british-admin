f2776b00e98775d6c48d3c92cdd11d4be7f614e4
shyam-nyros <kundurushyamsundhar4@gmail.com>
2026-02-01 12:28:54 +0530

    admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
 
 import App from "./App";
+import { store } from "./store";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster position="top-right" />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
